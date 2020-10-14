7527ec2f7f7483d2d26f0100ffa28dba3e6b889e
Kendra Erin <kendraerin@Home.localdomain>
2020-10-14 14:45:16 -0700

    First update, unfinished

--- lab4.py
+++ lab4.py
@@ -2,4 +2,22 @@
 #Michael Reed (msr248@nau.edu)
 
 
+import random
+
+def ask_row_col():
+	row =
+	col =
+
+
 def main():
+	board = []
+
+	randomize(board)
+	moves = 0
+	while not is_solved(board):
+		show(board)
+		(row, col) = ask_row_and_col()
+		touch(board, row, col)
+		moved += 1
+	show(board)
+	print(f"You won with {moves} moves!")
